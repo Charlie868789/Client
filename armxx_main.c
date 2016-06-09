d9d33da79aa6cfc3ed7c8e48316e6c4ef8de55b8
charlie <lingchen868789@hotmail.com>
2016-06-09 00:26:03 -0700

    2, Just add some structure about this programming

---
+++ armxx_main.c
@@ -0,0 +1,22 @@
+/*
+* Function: This is a main function,which is used to call other function to commnunicate with
+*           sensor node and Server.
+* Author  : Charlie Chen
+* Time    : 2016/June/9
+* Version : v1.0
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+#define DEBUG_CLIENT  1
+
+int main(int argc, int *argv[])
+{
+	//authority right
+
+	//checking the network status
+
+        //communicate with Server
+
+        //offline
+}
